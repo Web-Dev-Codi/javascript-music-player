d5bb208c0b4f74409fb03b4d49b00d7541182c41
Web-Dev-Codi <cordiscobrian@gmail.com>
2024-01-08 17:08:24 +0100

    Adding Play Functionality

--- script.js
+++ script.js
@@ -89,14 +89,36 @@ const playSong = (id) => {
 	const song = userData?.songs.find((song) => song.id === id);
 	audio.src = song.src;
 	audio.title = song.title;
+
+	if (
+		userData?.currentSong === null ||
+		userData?.currentSong.id !== song.id
+	) {
+		audio.currentTime = 0;
+	} else {
+		audio.currentTime = userData.songCurrentTime;
+	}
+	userData.currentSong = song;
+	playButton.classList.add("playing");
+
+	audio.play();
 };
+
+const pauseSong = () => {
+	userData.songCurrentTime = audio.currentTime;
+
+	playButton.classList.remove("playing");
+	audio.pause();
+};
+
+const playNextSong = () => {};
 
 const renderSongs = (array) => {
 	const songsHTML = array
 		.map((song) => {
 			return `
       <li id="song-${song.id}" class="playlist-song">
-      <button class="playlist-song-info">
+      <button class="playlist-song-info" onclick="playSong(${song.id})">
           <span class="playlist-song-title">${song.title}</span>
           <span class="playlist-song-artist">${song.artist}</span>
           <span class="playlist-song-duration">${song.duration}</span>
@@ -112,5 +134,17 @@ const renderSongs = (array) => {
 
 	playlistSongs.innerHTML = songsHTML;
 };
+
+const getCurrentSongIndex = () => userData?.songs.indexOf(userData.currentSong);
+
+playButton.addEventListener("click", () => {
+	if (userData?.currentSong === null) {
+		playSong(userData?.songs[0].id);
+	} else {
+		playSong(userData?.currentSong.id);
+	}
+});
+
+pauseButton.addEventListener("click", pauseSong);
 
 renderSongs(userData?.songs);
