af05eb30c477a7c3d0fa5422d765c936bc910c90
Web-Dev-Codi <cordiscobrian@gmail.com>
2024-01-09 16:16:26 +0100

    Updating script

--- script.js
+++ script.js
@@ -111,7 +111,33 @@ const pauseSong = () => {
 	audio.pause();
 };
 
-const playNextSong = () => {};
+const playNextSong = () => {
+	if (userData?.currentSong === null) {
+		playSong(userData?.songs[0].id);
+	} else {
+		const currentSongIndex = getCurrentSongIndex();
+		const nextSong = userData?.songs[currentSongIndex + 1];
+
+		playSong(nextSong.id);
+	}
+};
+
+const playPreviousSong = () => {
+	if (userData?.currentSong === null) return;
+	else {
+		const currentSongIndex = getCurrentSongIndex();
+		const previousSong = userData?.songs[currentSongIndex - 1];
+
+		playSong(previousSong.id);
+	}
+};
+
+const highlightCurrentSong = () => {
+	const playlistSongElements = document.querySelectorAll(".playlist-song");
+	const songToHighlight = document.getElementById(
+		`song-${userData?.currentSong?.id}`
+	);
+};
 
 const renderSongs = (array) => {
 	const songsHTML = array
@@ -146,5 +172,9 @@ playButton.addEventListener("click", () => {
 });
 
 pauseButton.addEventListener("click", pauseSong);
+
+nextButton.addEventListener("click", playNextSong);
+
+previousButton.addEventListener("click", playPreviousSong);
 
 renderSongs(userData?.songs);
